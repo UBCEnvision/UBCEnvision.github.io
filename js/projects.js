be04bfea3353da0aea32fb154234ee57625b6cfd
Victor Chiew <victor.yhchiew@gmail.com>
2017-06-02 16:41:47 -0700

    Navbar hides when scrolling down

---
+++ js/projects.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {  
+	// hide .navbar first
+		$("#navbar").hide();
+
+		// fade in .navbar
+		$(function () {
+			$(window).scroll(function () {
+	            // set distance user needs to scroll before we fadeIn navbar
+				if ($(this).scrollTop() > 100) 
+				{
+					$("#navbar").fadeIn();
+				} 
+
+				else 
+				{
+					$("#navbar").fadeOut();
+				}
+			});
+	});
+});
+//Doc idle test
+$(document).idle({
+  onIdle: function(){
+    $("#navbar").fadeOut();
+  },
+  idle: 1000
+});
