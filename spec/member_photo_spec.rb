e54b273caed64c326ffea196c95831d02b27410e
Josh Donaldson <donaldson.josh.96@gmail.com>
2018-10-01 23:40:39 -0700

    add square test back .... i dislike ruby

--- spec/member_photo_spec.rb
+++ spec/member_photo_spec.rb
@@ -5,6 +5,15 @@ require_all 'assets/images/members/*'
 describe "Member Photos" do
   let(:photos) {Dir["_site/assets/images/members/*"]}
 
+  it "should be square" do
+    photos.each do |photo|
+      image = image_loader(photo)
+      height, width = image.dimensions
+      expect(height.fdiv(width)).to be_within(0.05).of(1),
+        "Image is not square: height/width is ratio more than 5%: #{photo}"
+    end
+  end
+
   it "should be less than 500kb" do
     photos.each do |photo|
       image = image_loader(photo)
